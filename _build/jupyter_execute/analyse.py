35ebc536da55735315ff5c66705fe2760f00188a
luneauaymeric <aymeric.luneau@sciencespo.fr>
2024-01-10 11:05:08 +0100

    modification des titres

--- _build/jupyter_execute/analyse.py
+++ _build/jupyter_execute/analyse.py
@@ -15,10 +15,31 @@ import plotly.express as px #librairie pour faire des graphiques interactifs
 # In[2]:
 
 
-df0 = pd.read_csv("/home/aymeric/python-scripts/polls_wall_ladoumegue/data/convert_mur_ladoumegue_answer.csv", sep =",")
+rename_column = {
+      'Horodateur':'Horodateur',
+      'Au sein de quel(s) club(s) êtes-vous licencié.e ?':'Clubs',
+      'Au cours des mois de novembre et décembre 2023, combien de fois approximativement avez-vous grimpé à Ladoumègue ?': 'Frequentation',
+      'Aujourd’hui, quel est votre niveau à vue ? (on appelle “niveau à vue” le niveau le plus élevé des voies qu’on arrive à faire en tête au premier essai dans environ 75% des cas, quel que soit le profil et quelle que soit la salle)':'A_vue',
+      'Quelle est la cotation maximale dans laquelle vous essayez des voies (en moulinette ou en tête, sans forcément enchainer la voie)':'level_max',
+      'Selon vous, quel est le niveau de voie le plus fréquent sur le grand mur de Ladoumègue ?':'niveau_frequent',
+      'De manière plus générale, êtes-vous satisfait.e de la répartition des niveaux ?':'Satisfecit',
+      'Quel niveau aimeriez-vous rencontrer plus fréquemment ?':'Wish_level',
+      "Grimperiez-vous plus souvent à Ladoumègue s'il y avait plus de voies entre :":'Wish_level2',
+      'Actuellement, utilisez-vous le petit mur ?':'petit_mur',
+      "Actuellement, utilisez-vous l'espace bloc?":'bloc',
+      'Dans l’ensemble, j’ai trouvé que les cotations étaient':'surcotation',
+      'Dans l’ensemble, j’ai trouvé que les cotations étaient ': 'coherence',
+      "N'hésitez pas à utiliser le champ ci-dessous si vous souhaitez ajouter un commentaire. Vous pouvez également indiquer votre nom pour qu'on puisse revenir en discuter avec vous." : "Commentaires"
+      }
 
 
 # In[3]:
+
+
+df0 = pd.read_csv("/home/aymeric/python-scripts/polls_wall_ladoumegue/data/convert_mur_ladoumegue_answer.csv", sep =",")
+
+
+# In[4]:
 
 
 # On prend uniquement la colonne "clubs" pour compter le nombre d'individidus licenciés auprès de chaque club.
@@ -39,7 +60,15 @@ df = df0.merge(dfp, on = ["id"], how = "left")
 
 # ## Les clubs d'affiliation
 
-# In[4]:
+# In[5]:
+
+
+
+key = [k for k, v in rename_column.items() if v == 'Clubs'][0]
+print(f"Question posée :  {key}")
+
+
+# In[6]:
 
 
 fig, axs = plt.subplots(2, figsize=(15, 10));
@@ -62,49 +91,12 @@ fig.suptitle("Les clubs d'affiliation des répondant.es (une même personne peut
 #fig.show()
 
 
-# ## Satisfaction de la répartition des cotations
-
-# In[5]:
-
-
-d = df.groupby(["Satisfecit","level_max"]).agg(nb = ("id", "size")).reset_index()
-fig = px.treemap(d, path=["Satisfecit","level_max"], values='nb')
-fig
-
-
-# ## Répartition des niveaux max au sein des clubs
-# 
-# Pour rappel, une même personne peut être licenciée de plusieurs clubs.
-
-# In[6]:
-
-
-
-for n, col in enumerate(df1.Clubs2.unique()):
-    dtp = df.groupby([col,"level_max"]).agg(nb = (col, "sum")).reset_index()
-    fig = px.treemap(dtp, path=[col,"level_max"], values='nb', title = col)
-    fig.show()
-
-
-# ## Satisfaction de la distribution des niveaux de voies par club.
+# ## Le niveau des répondant.es
 
 # In[7]:
 
 
-
-for n, col in enumerate(df1.Clubs2.unique()):
-    dtp = df.groupby([col,"Satisfecit"]).agg(nb = ("id", "size")).reset_index()
-    dtp = dtp.loc[dtp[col]==1]
-    fig = px.treemap(dtp, path=[col,"Satisfecit"], values='nb', title = col)
-    fig.show()
-
-
-# ## Le niveau des répondant.es
-
-# In[8]:
-
-
-fig, axs = plt.subplots(4, figsize=(15, 15))
+fig, axs = plt.subplots(4, figsize=(15, 30))
 
 dtp1 = df.groupby(["level_max"]).agg(nb = ("id", "size")).reset_index()
 dtp1["freq"] = dtp1["nb"]/ df.id.nunique()*100
@@ -117,27 +109,159 @@ dtp4 = df.loc[df["Wish_level"]!= ""].groupby(["Wish_level"]).agg(nb = ("id", "si
 dtp4["freq"] = dtp4["nb"]/ df.id.nunique()*100
 
 for n, col in enumerate([dtp2, dtp1, dtp3, dtp4]):
+    fig.suptitle("Le niveau des répondant.es", fontsize=16);
 
-  if n == 0:
-    sns.barplot(x = 'A_vue', y = "nb", data=col, ax = axs[n])
-    axs[n].set_xlabel("Niveau à vue", fontsize = 12)
-  elif n == 1:
-    sns.barplot(x = 'level_max', y = "nb", data=col, ax = axs[n])
-    axs[n].set_xlabel("Niveau max", fontsize = 12)
-  elif n == 2:
-    sns.barplot(x = 'niveau_frequent', y = "nb", data=col, ax = axs[n])
-    axs[n].set_xlabel("Cotation la plus fréquente", fontsize = 12)
-  else:
-    sns.barplot(x = 'Wish_level', y = "nb", data=col, ax = axs[n])
-    axs[n].set_xlabel("Cotation souhaitée", fontsize = 12)
-  axs[n].set_ylabel("Effectif absolu", fontsize = 12)
+    if n == 0:
+        key = [k for k, v in rename_column.items() if v == "A_vue"][0]
+        axs[n].set_title(key, fontsize =14)
+        sns.barplot(x = 'A_vue', y = "nb", data=col, ax = axs[n])
+        axs[n].set_xlabel("Niveau à vue", fontsize = 12)
+        
+    elif n == 1:
+        key = [k for k, v in rename_column.items() if v == "level_max"][0]
+        axs[n].set_title(key, fontsize =14)
+        sns.barplot(x = 'level_max', y = "nb", data=col, ax = axs[n])
+        axs[n].set_xlabel("Niveau max", fontsize = 12)
+        
+    elif n == 2:
+        key = [k for k, v in rename_column.items() if v == "niveau_frequent"][0]
+        axs[n].set_title(key, fontsize =14)
+        sns.barplot(x = 'niveau_frequent', y = "nb", data=col, ax = axs[n])
+        axs[n].set_xlabel("Cotation la plus fréquente", fontsize = 12)
+    else:
+        key = [k for k, v in rename_column.items() if v == "Wish_level"][0]
+        axs[n].set_title(key, fontsize =14)
+        sns.barplot(x = 'Wish_level', y = "nb", data=col, ax = axs[n])
+        axs[n].set_xlabel("Cotation souhaitée", fontsize = 12)
+    axs[n].set_ylabel("Effectif absolu", fontsize = 12)
 
 
 
-fig.suptitle("Le niveau des répondant.es", fontsize=16);
 
 
 #fig.show()
+
+
+# ## Répartition des niveaux à vue au sein des clubs
+# 
+# Pour rappel, une même personne peut être licenciée de plusieurs clubs.
+
+# In[8]:
+
+
+key = [k for k, v in rename_column.items() if v == "A_vue"][0]
+print(key)
+
+for n, col in enumerate(df1.Clubs2.unique()):
+    dtp = df.groupby([col,"A_vue"]).agg(nb = (col, "sum")).reset_index()
+    fig = px.treemap(dtp, path=[col,"A_vue"], values='nb', title = f"Les niveaux à vue des adhérent.es de {col}")
+    fig.show()
+
+
+# ## Répartition des niveaux max au sein des clubs
+# 
+
+# In[9]:
+
+
+key = [k for k, v in rename_column.items() if v == "level_max"][0]
+print(key)
+
+for n, col in enumerate(df1.Clubs2.unique()):
+    dtp = df.groupby([col,"level_max"]).agg(nb = (col, "sum")).reset_index()
+    fig = px.treemap(dtp, path=[col,"level_max"], values='nb', title = f"Les niveaux max des adhérent.es de {col}")
+    fig.show()
+
+
+# ## Satisfaction de la répartition des cotations
+#  
+#  ### Satisfaction en fonction du niveau à vue
+
+# In[10]:
+
+
+key1 = [k for k, v in rename_column.items() if v == "Satisfecit"][0]
+key2 = [k for k, v in rename_column.items() if v == "A_vue"][0]
+print(f"Croisement des questions suviantes : ")
+print(f"1) {key1}")
+print(f"2) {key2}")
+
+
+d = df.groupby(["Satisfecit","A_vue"]).agg(nb = ("id", "size")).reset_index()
+fig = px.treemap(d, path=["Satisfecit","A_vue"], values='nb', title = "La satisfaction et le niveau à vue")
+fig
+
+
+# ### Satisfaction en fonction du niveau max
+
+# In[11]:
+
+
+key1 = [k for k, v in rename_column.items() if v == "Satisfecit"][0]
+key2 = [k for k, v in rename_column.items() if v == "level_max"][0]
+print(f"Croisement des questions suviantes : ")
+print(f"1) {key1}")
+print(f"2) {key2}")
+
+d = df.groupby(["Satisfecit","level_max"]).agg(nb = ("id", "size")).reset_index()
+fig = px.treemap(d, path=["Satisfecit","level_max"], values='nb', title = "La satisfaction et le niveau max")
+fig
+
+
+# ### Satisfaction de la distribution des niveaux de voies par club.
+
+# In[12]:
+
+
+key1 = [k for k, v in rename_column.items() if v == "Satisfecit"][0]
+
+print(key1)
+
+for n, col in enumerate(df1.Clubs2.unique()):
+    dtp = df.groupby([col,"Satisfecit"]).agg(nb = ("id", "size")).reset_index()
+    dtp = dtp.loc[dtp[col]==1]
+    fig = px.treemap(dtp, path=[col,"Satisfecit"], values='nb', title = col)
+    fig.show()
+
+
+# ## Les personnes non satisfaites
+#  
+#  ### Les personnes non satisfaites et le niveau qu'elles souhaitent rencontrer plus souvent
+
+# In[13]:
+
+
+key1 = [k for k, v in rename_column.items() if v == "Wish_level"][0]
+
+print(key1)
+df2 = df.loc[df["Satisfecit"]=="Non"]
+
+
+dtp = df2.groupby(["Satisfecit", "Wish_level"]).agg(nb = ("id", "size")).reset_index()
+#dtp = dtp.loc[dtp[col]==1]
+fig = px.treemap(dtp, path=["Satisfecit", "Wish_level"], values='nb')
+fig.show()
+
+
+# ### Les personnes non satisfaites et le niveau qu'elles souhaitent rencontrer plus souvent en fonction du club
+
+# In[14]:
+
+
+key1 = [k for k, v in rename_column.items() if v == "Wish_level"][0]
+
+print(key1)
+df2 = df.loc[(df["Satisfecit"]=="Non")]
+df2 = df2.loc[~df2["Satisfecit"].isna()]
+
+for n, col in enumerate(df1.Clubs2.unique()):
+    dtp = df2.groupby([col,"Wish_level"]).agg(nb = ("id", "size")).reset_index()
+    dtp = dtp.loc[dtp[col]==1]
+    if len(dtp)> 0 :
+        fig = px.treemap(dtp, path=[col,"Wish_level"], values='nb', title = col)
+        fig.show()
+    else:
+        pass
 
 
 # In[ ]:
